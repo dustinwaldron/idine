a950800d5b48533979799a8abf8e717d83558af0
dustinwaldron <waldrd11@gmail.com>
2020-10-20 22:09:43 -0400

    initial commit

---
+++ idine/idineApp.swift
@@ -0,0 +1,17 @@
+//
+//  idineApp.swift
+//  idine
+//
+//  Created by Waldron, Dustin on 10/20/20.
+//
+
+import SwiftUI
+
+@main
+struct idineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
